ef9e1171bb0297dacacb45624de73867e08c3007
Shubham Kamdi <shubhamvkamdi@gmail.com>
2025-10-04 14:58:16 -0700

    Initial Commit

---
+++ Chart/ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartApp.swift
+//  Chart
+//
+//  Created by Shubham Kamdi on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chart
+//
+//  Created by Shubham Kamdi on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
